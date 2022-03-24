f099b42d80a22c9e08a3d296e16e758f75fc0004
Floxptrde <102305057+Floxptrde@users.noreply.github.com>
2022-03-25 00:02:53 +0100

    Add files via upload

--- commands/walletnull.js
+++ commands/walletnull.js
@@ -5,10 +5,10 @@ module.exports = {
         if(!message.member.roles.cache.has('791199098510114837')) return;
         message.mentions.user.map(user => {
             //console.log(rname.name);
-            let coinValue = Object.values(coin.filter({id: user.id}).find('coins').value())[1];
+            let coinValue = Object.values(coin.filter({id: user.user.id}).find('coins').value())[1];
             let botCoinValue = Object.values(coin.filter({id: "953981215407501363"}).find('coins').value())[1];
 
-            coin.find({id: user.id}).assign({id: user.id, coins: 1}).write();
+            coin.find({id: user.user.id}).assign({id: user.user.id, coins: 1}).write();
             coin.find({id: "953981215407501363"}).assign({id: "953981215407501363", coins: botCoinValue += coinValue}).write();
             message.reply('votre wallet a été réinitialisé, votre solde a été envoyé au bot');
         });
