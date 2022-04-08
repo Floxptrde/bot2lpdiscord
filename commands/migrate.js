23c6254859cb03ef2a22d5e978c6a7d18d382961
Floxptrde <102305057+Floxptrde@users.noreply.github.com>
2022-04-08 11:49:12 +0200

    Add files via upload

--- commands/migrate.js
+++ commands/migrate.js
@@ -3,7 +3,17 @@ module.exports = {
     description: "Commande réservée, permet de récupérer les données du bot __!migrate__",
     execute(message, client, coin, roleBase, MessageEmbed, ChanWelcome, ChanRegles, ChanBot){
         if (!message.member.permissions.has('MANAGE_GUILD')) return;
-        client.channels.cache.get(`${ChanBot}`).send("coin");
-        client.channels.cache.get(`${ChanBot}`).send("roleBase");
+        let StringCoin = '';
+        for(const n of coin){
+            StringCoin = StringCoin+`\n{\nid: "${n.id}",\ncoins: ${n.coins}\n},`;
+        }
+
+        let StringRoles = '';
+        for(const n of roleBase){
+            StringCoin = StringCoin+`\n{\nid: "${n.id}",\nname: "${n.name}"\nprice: ${n.price}\nlevel: ${n.level}\n},`;
+        }
+
+        client.channels.cache.get(`${ChanBot}`).send(StringCoin);
+        client.channels.cache.get(`${ChanBot}`).send(StringRoles);
     }
 }
